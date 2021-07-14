96e5bf6b2ad1e91edb3ad8d8aadd9e199f0a861a
Ryan Wholey <ryan@takescoop.com>
2021-07-14 11:18:24 -0600

    Ensure typescript ok in test file

--- __tests__/copy.test.ts
+++ __tests__/copy.test.ts
@@ -1,18 +1,18 @@
-import path from 'path'
-import fs from 'fs'
+import * as path from 'path'
+import * as fs from 'fs'
 
 import {copyFile} from '../src/copy'
 
 process.env.CI &&
   test('it downloads a file', async () => {
     const [owner, repo] = (process.env.GITHUB_REPOSITORY as string).split('/')
-    
+
     const filePath = await copyFile({
       owner,
       repo,
       srcPath: 'fixtures/.tflint.hcl',
       ref: 'main',
-      dstPath: path.join(process.env.RUNNER_TEMP, '.tflint.hcl')
+      dstPath: path.join(process.env.RUNNER_TEMP as string, '.tflint.hcl')
     })
 
     expect(await fs.promises.readFile(filePath, 'utf8')).toEqual(
