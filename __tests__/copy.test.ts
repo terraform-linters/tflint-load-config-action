9681d124d6eb2ed08bf248d858e69cb384f7921b
Ben Drucker <bvdrucker@gmail.com>
2026-03-13 18:35:30 -0700

    fix __dirname for ESM jest

--- __tests__/copy.test.ts
+++ __tests__/copy.test.ts
@@ -1,5 +1,8 @@
 import * as path from 'path'
 import * as fs from 'fs'
+import {fileURLToPath} from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 import {copyFile} from '../src/copy'
 
